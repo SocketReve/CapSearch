86d134f7ad0cf2ad1c4b5fb10fd661889acbd81e
Luca Reverberi <thereve@gmail.com>
2010-02-23 12:16:09 +0100

    CapSearch v1

---
+++ pgm.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+__author__= "Reverberi Luca"
+
+'''    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+'''
+
+from gui import *
+from string import upper
+import sqlite3
+import sys
+
+
+class rendering(QtGui.QDialog):
+	''' Classe della finestra'''
+	def __init__(self):
+		self.active = True
+		self.app = QtGui.QApplication([]) 							# Crea un'applicazione Qt, ...
+		self.Dialog = QtGui.QDialog() 								# ... un dialogo, ...
+		self.ui = Ui_Dialog() 									# ... e una user interface.
+		self.ui.setupUi(self.Dialog) 								# Infila il dialog nella user interface
+		self.app.setQuitOnLastWindowClosed(False)
+
+		self.createTrayIcon()
+		
+		# Segnali:
+		self.app.connect(self.ui.buttonCerca,QtCore.SIGNAL("clicked()"),self.actionButtonSearch)
+		self.app.connect(self.ui.buttonExit,QtCore.SIGNAL("clicked()"),self.actionButtonExit)		
+		self.Systray.activated.connect(self.iconActivated)
+		#
+        	
+        	
+	def runInterface(self):
+		self.Dialog.show()
+		return self.app.exec_() 
+	
+        def createTrayIcon(self):
+        	icona = QtGui.QIcon("cap.png")
+        	self.Systray=QtGui.QSystemTrayIcon(icona)
+        	self.Systray.show()
+
+	# Funzioni di segnali:
+	def actionButtonSearch(self):
+  		ricerca = readSql()
+  		self.ui.lineCap.setText(str(ricerca.query(self.ui.lineProv.text(),self.ui.lineComune.text(),self.ui.lineLocal.text())))
+  	
+  	def actionButtonExit(self):
+  		sys.exit(self.app.exec_())
+  	#
+
+  	def iconActivated(self, reason):
+  		if reason == QtGui.QSystemTrayIcon.Trigger:
+  			if self.active == True:
+  				self.Dialog.hide()
+  				self.active = False
+  				
+  			elif self.active == False:
+  				self.Dialog.show()
+  				self.active = True
+
+
+class readSql():
+	def query(self,provincia,comune,localita=None):
+		con = sqlite3.connect("databaseCap.db")
+		cur = con.cursor()
+		provincia=str(provincia)
+		comune=str(comune)
+		localita=str(localita)
+		
+		if localita=="":
+			cur.execute("SELECT capi_cap FROM tab_cap WHERE prov_cap = '"+upper(provincia)+"' AND comu_cap = '"+upper(comune)+"'")
+		else:
+			cur.execute("SELECT capi_cap FROM tab_cap WHERE prov_cap = '"+upper(provincia)+"' AND comu_cap = '"+upper(comune)+"' AND fraz_cap = '"+upper(localita)+"' OR topo_cap = '"+upper(localita)+"'")
+		var = cur.fetchone()
+		
+		if var==None:
+			return "Errore"
+		else:
+			return var[0]
+
+if __name__ == "__main__":
+	var = rendering()
+	var.runInterface() 										# mostra il dialog precedentemente creato
